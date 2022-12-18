d919a2f43bf6be39121fb689979791b2b5621d85
kmuali <kmuali>
2022-12-19 00:24:40 +0200

    OK

--- cpu-vhdl/cu.vhd
+++ cpu-vhdl/cu.vhd
@@ -24,7 +24,7 @@ end entity cu;
 
 architecture behav of cu is
   signal step : std_logic_vector (2-1 downto 0); -- step counter
-  signal inst: std_logic_vector (4-1 downto 0);
+  signal inst_oc: std_logic_vector (4-1 downto 0); -- opcode
   signal inst_rs, inst_rt, inst_rd, inst_op: std_logic_vector(3-1 downto 0);
   signal inst_RRI_imm, inst_RI_imm: std_logic_vector(16-1 downto 0);
   signal inst_RI_UJ_addr, inst_CJ_addr: std_logic_vector(8-1 downto 0);
@@ -32,7 +32,7 @@ architecture behav of cu is
   signal FR: std_logic_vector (3-1 downto 0);
 begin
   -- splitting instruction into meaningful parts
-  inst <= IR(15 downto 12);
+  inst_oc <= IR(15 downto 12);
   inst_rs <= IR(11 downto 9);
   inst_rt <= IR(8 downto 6);
   inst_rd <= IR(5 downto 3);
@@ -49,27 +49,27 @@ begin
   step3 <= step(0) and step(1);
 
 
-  with inst select
+  with inst_oc select
     alu_B <= reg_R2 when x"1" | x"A" | x"B" | x"C", -- arith RRR and cond jumping
              inst_RRI_imm  when others; -- arith RRI
 
-  with inst select
+  with inst_oc select
     alu_S <= inst_op when x"1",
              o"0" when x"2", -- add inst_RRI_imm
              inst_op(alu_S'range) when x"4" | x"5" | x"6", -- and, or, xor inst_RRI_imm
              o"1" when others; -- sub inst_RRI_imm, jumping comparasions xA xB xC
 
-  with inst select
+  with inst_oc select
     reg_W <= mem_R when x"8", -- load mem
              inst_RI_imm when x"7", -- load imm
              alu_F when others; -- arith all types 
 
-  with inst select
+  with inst_oc select
     reg_WA <= inst_rd when x"1", -- arith RRR
               inst_rs when x"7" | x"8", -- load imm, load mem
               inst_rt when others; -- arith RRI
 
-  with inst select
+  with inst_oc select
     PC_new <= inst_RI_UJ_addr when x"F",
              inst_CJ_addr when others;
 
@@ -78,15 +78,15 @@ begin
   reg_RA1 <= inst_rs;
   reg_RA2 <= inst_rt;
 
-  with inst select
+  with inst_oc select
     mem_E <= '1' when x"9",
              '0' when others;
 
-  with inst select
+  with inst_oc select
     reg_E <= step2 when x"1" | x"2" | x"3" | x"4" | x"5" | x"6" | x"7" | x"8",
              '0' when others;
 
-  with inst select
+  with inst_oc select
     PC_changed <= step3 and FR(0) when x"A", -- equal
                   step3 and not FR(2) and not FR(0) when x"B", -- greater (not equal nor less)
                   step3 and FR(2) when x"C", -- less
@@ -95,7 +95,7 @@ begin
                   '1' when x"F",
                   '0' when others;
 
-  with inst select
+  with inst_oc select
     exe_done <= step3 when x"A" | x"B" | x"C",
                 step1 when x"D" | x"E" | x"F",
                 step2 when others;
@@ -108,7 +108,7 @@ begin
       else
         step <= "00";
       end if;
-      case inst is
+      case inst_oc is
        when x"1" | x"2" | x"3" | x"4" | x"5" | x"6" | x"A" | x"B" | x"C" => -- arith and cond jump
          case alu_F is
            when x"0000" => FR(0) <= '1';
