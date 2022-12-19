c20d2ffab0f87900155fe943ea550a3b378ec185
kmuali <kmuali>
2022-12-19 13:53:20 +0200

    OK

--- cpu-vhdl/alu16.vhd
+++ cpu-vhdl/alu16.vhd
@@ -14,33 +14,35 @@ entity alu16 is
 end entity alu16;
 
 architecture behav of alu16 is
+  component mul16 is
+    port (
+    A, B: in std_logic_vector(16-1 downto 0);
+    P: out std_logic_vector(16-1 downto 0)
+  );
+  end component;
   component div16 is
     port (
     A, B: in std_logic_vector(16-1 downto 0);
     Q: out std_logic_vector(16-1 downto 0)
   );
   end component;
+
   signal add: std_logic_vector(16 downto 0);
-  signal div_Q, mul, abs_A, abs_B: std_logic_vector (16-1 downto 0);
-  signal mul_div_sign : std_logic;
+  signal div_Q, mul_P: std_logic_vector (16-1 downto 0);
+
 begin
-  with A(15) select
-    abs_A <= A when '0',
-             0-A  when others;
-  with B(15) select
-    abs_B <= B when '0',
-             0-B  when others;
-             
+
   add <= ('0'&A) + ('0'&B);
   C <= add(16) and not (s(0) or s(1) or s(2));
-  mul <= abs_A(8-1 downto 0) * abs_B(8-1 downto 0);
-  div16_1: div16 port map(abs_A, abs_B, div_Q);
-  mul_div_sign <= A(15) XOR B(15);
+
+  div16_1: div16 port map(A, B, div_Q);
+  mul16_1: mul16 port map(A, B, mul_P);
+
   with S select -- function
     F <= add(F'range) when o"0",
          A - B when o"1",
-         (mul_div_sign)&mul(14 downto 0) when o"2",
-         (mul_div_sign)&div_Q(14 downto 0) when o"3",
+         mul_P when o"2",
+         div_Q when o"3",
          A and B when o"4",
          A or B  when o"5",
          A xor B when o"6",
--- cpu-vhdl/div16.vhd
+++ cpu-vhdl/div16.vhd
@@ -15,24 +15,37 @@ end entity div16;
 
 
 architecture behav of div16 is
-  type mat is array(0 to 15) of std_logic_vector(16 downto 0);
+  type mat is array(0 to 14) of std_logic_vector(A'range);
   signal top, diff: mat;
+  signal us_A, us_B : std_logic_vector (A'range);
 begin
 
+  Q(15) <= A(15) xor B(15); -- sign
+  
 
-  top(0) <= x"0000"&A(15);
-  diff(0) <= top(0) - ("0"&B);
+  with A(15) select
+    us_A <= 0 - A when '1',
+            A when others;
 
-  gsub: for i in 1 to 15 generate
-    top(i)(16) <= '0';
-    top(i)(0) <= A(15-i);
-    with diff(i - 1)(16) select
-      top(i)(15 downto 1) <= diff(i - 1)(14 downto 0) when '0',
-                             top(i - 1)(14 downto 0) when others;
-    diff(i) <= top(i) - ("0"&B);
+  with B(15) select
+    us_B <= 0 - B when '1',
+            B when others;
+
+  top(0) <= o"00000" & us_A(14);
+  diff(0) <= top(0) - us_B;
+
+  gsub: for i in 1 to 14 generate
+    top(i)(15) <= '0';
+    top(i)(0) <= us_A(14-i);
+    with diff(i - 1)(16-1) select -- sign control MUX
+      top(i)(14 downto 1) <= diff(i - 1)(13 downto 0) when '0',
+                             top(i - 1)(13 downto 0) when others;
+    diff(i) <= top(i) - us_B;
     end generate gsub;
-  gQ: for i in 0 to 15 generate
-    Q(15 - i) <= NOT diff(i)(16);
-  end generate gQ;
+
+    gQ: for i in 0 to 14 generate
+      Q(14 - i) <= NOT diff(i)(15);
+    end generate gQ;
+ 
 
 end behav;
